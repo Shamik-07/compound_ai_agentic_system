a6980fcc95e0ced470914576bb06e396c9f4bba5
shamik <39588365+Shamik-07@users.noreply.github.com>
2024-12-16 14:42:36 +0530

    typing annotations in utils.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import json
 from pathlib import Path
 from shutil import rmtree
+from textwrap import dedent
+from typing import Any
 
 import wikipedia
 from dotenv import load_dotenv
@@ -9,7 +11,8 @@ from openai import OpenAI
 _ = load_dotenv()
 openai_client = OpenAI()
 
-def moderate_content(text):
+
+def moderate_content(text: str) -> str:
     """Use this function to moderate messages.
 
     Args:
@@ -18,35 +21,43 @@ def moderate_content(text):
     Returns:
         bool: JSON string of True or False.
     """
-    response = openai_client.moderations.create(model="omni-moderation-latest", input=text)
+    response = openai_client.moderations.create(
+        model="omni-moderation-latest", input=text
+    )
     return json.dumps(response.results[0].flagged)
 
 
-def create_finance_reports_dir():
+def create_finance_reports_dir() -> Path:
     reports_dir = Path(__file__).parent.joinpath("finance_agent", "reports")
     if reports_dir.exists():
         rmtree(path=reports_dir, ignore_errors=True)
     reports_dir.mkdir(parents=True, exist_ok=True)
     return reports_dir
 
-def search_on_wikipedia(query):
+
+def search_on_wikipedia(query: str) -> Any | str | Any:
     try:
         content = wikipedia.page(title=query, auto_suggest=False).summary
         return content
     except wikipedia.DisambiguationError as err:
-        return f"Your query resulted to the following topics: {err.options}. "+\
-        "Which one do you want to know about?"
+        return (
+            f"Your query resulted to the following topics: {err.options}. "
+            + "Which one do you want to know about?"
+        )
     except wikipedia.PageError:
-        if len(search_result:= wikipedia.search(query)):
-            return dedent(f"The query didn't match an exact page but\
-            these are the closest search results: {search_result}")
+        if len(search_result := wikipedia.search(query)):
+            return dedent(
+                f"The query didn't match an exact page but\
+            these are the closest search results: {search_result}"
+            )
         else:
-            return f"No search results for: {query}. "+\
-            "Please try and be more specific."
-    return
+            return (
+                f"No search results for: {query}. " + "Please try and be more specific."
+            )
 
-def delete_exisiting_chat_history():
-    filepath=Path(__file__).parent.joinpath("agent_storage.db")
+
+def delete_exisiting_chat_history() -> None:
+    filepath = Path(__file__).parent.joinpath("agent_storage.db")
     if filepath.exists():
         filepath.unlink()
     return
